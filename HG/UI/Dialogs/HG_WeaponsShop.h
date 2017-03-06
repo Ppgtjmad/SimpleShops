d4ad19c9b0e5e25068e3df296769494759672bd9
Ppgtjmad <ppgtjmadakapeepee@outlook.com>
2017-03-06 22:05:03 +0100

    03/06/2017 #3

--- HG/UI/Dialogs/HG_WeaponsShop.h
+++ HG/UI/Dialogs/HG_WeaponsShop.h
@@ -168,7 +168,7 @@ class HG_WeaponsShop
 		{
 			idc = HG_WEAPONS_MC_IDC;
 			tooltip = "$STR_HG_DLG_MC_TOOLTIP";
-			onButtonClick = "hint format[(localize 'STR_HG_DLG_MC'),if((getNumber(missionConfigFile >> 'CfgClient' >> 'enableSave')) isEqualTo 1) then {[(profileNamespace getVariable 'HG_Save'),true] call HG_fnc_currencyToText} else {[(player getVariable 'HG_myCash'),true] call Client_fnc_currencyToText}]";
+			onButtonClick = "hint format[(localize 'STR_HG_DLG_MC'),if((getNumber(missionConfigFile >> 'CfgClient' >> 'enableSave')) isEqualTo 1) then {[(profileNamespace getVariable 'HG_Save'),true] call HG_fnc_currencyToText} else {[(player getVariable 'HG_myCash'),true] call HG_fnc_currencyToText}]";
 			x = 30.5 * GUI_GRID_W + GUI_GRID_X;
 			y = 2.5 * GUI_GRID_H + GUI_GRID_Y;
 			w = 3 * GUI_GRID_W;
